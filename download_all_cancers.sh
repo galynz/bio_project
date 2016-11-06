00a8414047489d517be723e18d904e8c3ef35ab5
Gal Yankovitz <galyankovitz@compute-0-12.local>
2016-11-06 09:36:42 +0200

    added a script that updates the manifest before the download

--- download_all_cancers.sh
+++ download_all_cancers.sh
@@ -2,6 +2,8 @@
 for cancer in $(cat cancer_list.txt); do
     echo $cancer;
     cd $cancer
-    gdc-client download -m *.tsv
+    cp ~/bio_project/update_gdc_manifest.py .
+    python2.7 update_gdc_manifest.py gdc_manifest.2016-10-*.tsv
+    gdc-client download -m *2016-10-*.tsv.final -t ~/bio_project/token.txt
     cd ..
 done

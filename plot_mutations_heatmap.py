56787626c360794d283229d82266164c3f787eed
galynz <galynz@gmail.com>
2016-11-28 09:13:54 +0200

    added logs

--- plot_mutations_heatmap.py
+++ plot_mutations_heatmap.py
@@ -134,6 +134,7 @@ def parse_vcf(vcf_path, samples_dict):
                             biotype = i[fields.index("BIOTYPE")]
                             priority = BIOTYPE_PRIORITY[biotype]
                             samples_dict[sample].add_germline_mutation(hugo_code, priority)
+                            logger.debug("added gene %s with priority %d to sample %s", hugo_code, priority, sample)
                 elif line.startswith("##INDIVIDUAL"):
                     sample = re.compile(r'INDIVIDUAL=<NAME=(TCGA-[A-Z0-9-]*),').findall(line)[0]
                     if not samples_dict.has_key(sample):

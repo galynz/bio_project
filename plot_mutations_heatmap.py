c93d8ff50f2ba87945978e3054c95b39a8bf425e
galynz <galynz@gmail.com>
2016-11-28 09:29:58 +0200

    fixed a bug

--- plot_mutations_heatmap.py
+++ plot_mutations_heatmap.py
@@ -130,11 +130,12 @@ def parse_vcf(vcf_path, samples_dict):
                     if line.find("germline_risk") > -1:
                         info = [i.split("|") for i in line[line.find("CSQ=")+len("CSQ="):line.find(" ", line.find("CSQ=")+len("CSQ="))].split(",")]
                         for i in info:
-                            hugo_code = i[fields.index("SYMBOL")]
-                            biotype = i[fields.index("BIOTYPE")]
-                            priority = BIOTYPE_PRIORITY[biotype]
-                            samples_dict[sample].add_germline_mutation(hugo_code, priority)
-                            logger.debug("added gene %s with priority %d to sample %s", hugo_code, priority, sample)
+                            if len(i) == len(info):
+                                hugo_code = i[fields.index("SYMBOL")]
+                                biotype = i[fields.index("BIOTYPE")]
+                                priority = BIOTYPE_PRIORITY[biotype]
+                                samples_dict[sample].add_germline_mutation(hugo_code, priority)
+                                logger.debug("added gene %s with priority %d to sample %s", hugo_code, priority, sample)
                 elif line.startswith("##INDIVIDUAL"):
                     sample = re.compile(r'INDIVIDUAL=<NAME=(TCGA-[A-Z0-9-]*),').findall(line)[0]
                     if not samples_dict.has_key(sample):

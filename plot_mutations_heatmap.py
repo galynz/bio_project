4d5be5ee238e101b5cafc9387b58826cbe502204
galynz <galynz@gmail.com>
2016-12-11 14:39:18 +0200

    added a print

--- plot_mutations_heatmap.py
+++ plot_mutations_heatmap.py
@@ -272,6 +272,7 @@ def plot_heatmap(samples_dict, output_path, cancer, df, genes):
 def plot_clustered_heatmap(df, genes_list, cancer, output_path):
     # Build nxm matrix (n samples, m genes)
     X = df[genes_list].as_matrix()
+    print X.shape
     
     Z = linkage(X, method='complete', metric='hamming')
     c, coph_dists = cophenet(Z, pdist(X))
